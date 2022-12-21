a9ac2f964005de4dbad6a11365c60a64ee59dd63
Manu S Mohan <manu.mohan@experionglobal.local>
2022-12-21 16:48:01 +0530

    added register

--- client/IMS-app/src/components/register_component/Register.jsx
+++ client/IMS-app/src/components/register_component/Register.jsx
@@ -1,168 +1,145 @@
-import React from "react";
 import { Field, Formik, Form, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import "./register.css";
-// import AuthService from "../modules/authservice";
+import React from "react";
+import * as Yup from 'yup';
+import accountsService from "../../services/accountsService"
+import './register.css'
 
-const Register = () => {
+// import styles from './Register.module.css'
+function Register() {
   return (
     <div className="maindiv">
-      <div className="home">
-        <div className="pic">
-          <h1>
-            <center>Register</center>
-          </h1>
-          {/* <p>Volup amet magna clita tempor. Tempor sea eos vero ipsum. Lorem lorem sit sed elitr sed kasd et</p> */}
-        </div>
-        <div className="container">
           <div className="page">
-            {/* <div className={styles.reg} */}
-            <div>
+            <h1>Student Details </h1> 
+        
               <Formik
-                initialValues={{
-                  name: "",
-                  email: "",
-                  phone: "",
-                  address: "",
-                  dob: "",
-                  password: "",
-                }}
-                validationSchema={Yup.object().shape({
-                  email: Yup.string().email().required("Email is required"),
-                  password: Yup.string().required("Password is required"),
-                })}
-                // onSubmit={async (values, { setSubmitting }) => {
-                //   var service = new AuthService();
-                //   var result = await service.register(values);
-                //   if (!result.success) {
-                //     alert(result.errors[0]);
-                //     return;
-                //   }
+                initialValues={{  name: "",pname:"",dob:"",pdob:"",phone:"",address:"",email:"",          password:"",
+                    occupation:"",relationship:"" ,pemail:"",ppassword:"",pphone:"",branch:"" }}
+                validationSchema={
+                  Yup.object().shape({
+                    email: Yup.string().email().required('Email is required'),
+                    password: Yup.string().required('Password is required'),
+                    name: Yup.string().required('Name is required'),
+                    pname: Yup.string().required('Name is required'),
+                    dob: Yup.string().required('DOB is required'),
+                    pdob: Yup.string().required('Parent DOB is required'),
+                    phone: Yup.string().required('Phone is required'),
+                    address: Yup.string().required('Address is required'),
+                    occupation: Yup.string().required('Occupation is required'),
+                    relationship: Yup.string().required('Relationship is required'),
+                    pemail: Yup.string().required('Parent email is required'),
+                    ppassword: Yup.string().required('Parent  password  is required'),
+                    pphone: Yup.string().required('Parent phone is required'),
+                    branch: Yup.string().required('Branch is required'),
+                    
+                  })
+                }
+                onSubmit={async (values, { setSubmitting }) => {
+                  var service = new accountsService();
+                  var result = await service.register(values);
+                  if (!result.success) {
+                    alert(result.errors[0]);
+                    return;
+                  }
 
-                //   alert("register Success");
-                //   // localStorage.setItem('token', res.data);
-                // }}
+                  alert('register Success');
+                  // localStorage.setItem('token', result.data);
+                }}
               >
                 {({ isSubmitting }) => (
                   <Form>
-                    <div className="mb-3">
-                      <label htmlFor="name" className="form-label">
-                        Name
-                      </label>
-                      <Field
-                        type="text"
-                        name="name"
-                        id="name"
-                        className="form-control"
-                        placeholder="Name"
-                        aria-describedby="helpId"
-                      />
+                    <div class="mb-3">
+                      <label for="name" class="form-label">Name</label>
+                      <Field type="text" name="name" id="name" class="form-control" placeholder="Name" aria-describedby="helpId" />
                     </div>
-                    <div className="mb-3">
-                      <label htmlFor="email" className="form-label">
-                        Email
-                      </label>
-                      <Field
-                        type="email"
-                        className="form-control"
-                        name="email"
-                        id="email"
-                        aria-describedby="emailHelpId"
-                        placeholder="abc@mail.com"
-                      />
-                      <ErrorMessage
-                        name="email"
-                        component="div"
-                        className="text-danger"
-                      />
+                    <div class="mb-3">
+                      <label for="email" class="form-label">Email</label>
+                      <Field type="email" class="form-control" name="email" id="email" aria-describedby="emailHelpId" placeholder="abc@mail.com" />
+                      <ErrorMessage name="email" component="div" className='text-danger' />
                     </div>
-                    <div className="mb-1 ">
-                      <label htmlFor="phone" className="form-label">
-                        Phone No.{" "}
-                      </label>
-                      <Field
-                        type="text"
-                        name="phone"
-                        id="phone"
-                        className="form-control"
-                        aria-describedby="helpId"
-                      />
+                    <div class="mb-1 ">
+                      <label for="phone" class="form-label">Phone No. </label>
+                      <Field type="text" name="phone" id="phone" class="form-control" aria-describedby="helpId" />
+                      <ErrorMessage name="phone" component="div" className='text-danger' />                   
                     </div>
-                    <div className="mb-3">
-                      <label htmlFor="dob" className="form-label">
-                        DOB
-                      </label>
-                      <Field
-                        type="date"
-                        name="dob"
-                        id="dob"
-                        className="form-control"
-                        aria-describedby="helpId"
-                      />
+                    <div class="mb-3">
+                      <label for="dob" class="form-label">DOB</label>
+                      <Field type="date" name="dob" id="dob" class="form-control" aria-describedby="helpId" />
+                      <ErrorMessage name="dob" component="div" className='text-danger' /> 
                     </div>
-                    <div className="mb-3">
-                      <label htmlFor="address" className="form-label">
-                        Address
-                      </label>
-                      <Field
-                        type="text"
-                        name="address"
-                        id="address"
-                        className="form-control"
-                        aria-describedby="helpId"
-                      />
+                    <div class="mb-3">
+                      <label for="address" class="form-label">Address</label>
+                      <Field type="text" name="address" id="address" class="form-control" aria-describedby="helpId" />
+                      <ErrorMessage name="address" component="div" className='text-danger' />                    
                     </div>
-                    <div className="mb-3">
-                      <label htmlFor="password" className="form-label">
-                        Password
-                      </label>
-                      <Field
-                        type="password"
-                        className="form-control"
-                        name="password"
-                        id="passord"
-                        placeholder="Password"
-                      />
-                      <ErrorMessage
-                        name="password"
-                        component="div"
-                        className="text-danger"
-                      />
+                    <div class="mb-3">
+                      <label for="branch" class="form-label">Branch</label>
+                      <Field type="text" class="form-control" name="branch" id="passord" />
+                      <ErrorMessage name="branch" component="div" className='text-danger' />
+                    </div>    
+                    <div class="mb-3">
+                      <label for="password" class="form-label">Password</label>
+                      <Field type="password" class="form-control" name="password" id="passord" placeholder="Password" />
+                      <ErrorMessage name="password" component="div" className='text-danger' />
+                    </div>                  
+                                     
+                    <div className="parent">
+                    
+                      <h1>Parent Details</h1>
+                      <div class="mb-3">
+                      <label for="name" class="form-label">Name</label>
+                      <Field type="text" name="pname" id="name" class="form-control" placeholder="Name" aria-describedby="helpId" />
+                      <ErrorMessage name="pname" component="div" className='text-danger' /> 
                     </div>
-                    <div className="mb-3">
-                      <label htmlFor="cpassword" className="form-label">
-                        Confirm Password
-                      </label>
-                      <Field
-                        type="password"
-                        className="form-control"
-                        name="cpassword"
-                        id="cpassword"
-                        placeholder="Confirm Password"
-                      />
-                      <ErrorMessage
-                        name="cpassword"
-                        component="div"
-                        className="text-danger"
-                      />
+                    <div class="mb-3">
+                      <label for="email" class="form-label">Email</label>
+                      <Field type="email" class="form-control" name="pemail" id="email" aria-describedby="emailHelpId" placeholder="abc@mail.com" />
+                      <ErrorMessage name="pemail" component="div" className='text-danger' />
+                    </div>
+                    <div class="mb-1 ">
+                      <label for="phone" class="form-label">Phone No. </label>
+                      <Field type="text" name="pphone" id="phone" class="form-control" aria-describedby="helpId" />
+                      <ErrorMessage name="pphone" component="div" className='text-danger' /> 
+                    </div>
+                    <div class="mb-3">
+                      <label for="dob" class="form-label">DOB</label>
+                      <Field type="date" name="pdob" id="dob" class="form-control" aria-describedby="helpId" />
+                      <ErrorMessage name="pdob" component="div" className='text-danger' /> 
+                    </div>
+                    <div class="mb-3">
+                      <label for="address" class="form-label">Address</label>
+                      <Field type="text" name="address" id="address" class="form-control" aria-describedby="helpId" />
+                      <ErrorMessage name="address" component="div" className='text-danger' /> 
+                    </div>
+                    <div class="mb-3">
+                      <label for="occupation" class="form-label">Occupation</label>
+                      <Field type="text" name="occupation" id="address" class="form-control" aria-describedby="helpId" />
+                      <ErrorMessage name="occupation" component="div" className='text-danger' /> 
+                    </div>
+                    <div class="mb-3">
+                      <label for="relationship" class="form-label">Relationship</label>
+                      <Field type="text" name="relationship" id="address" class="form-control" aria-describedby="helpId" />
+                      <ErrorMessage name="relationship" component="div" className='text-danger' /> 
+                    </div>
+                    <div class="mb-3">
+                      <label for="ppassword" class="form-label">Password</label>
+                      <Field type="ppassword" class="form-control" name="ppassword" id="passord" placeholder="Password" />
+                      <ErrorMessage name="ppassword" component="div" className='text-danger' />
+                    </div>                  
+                     
                     </div>
 
-                    <button
-                      type="submit"
-                      disabled={isSubmitting}
-                      className="btn btn-primary"
-                    >
-                      Submit
-                    </button>
+                    <button type="submit" disabled={isSubmitting} class="btn btn-primary">Submit</button>
                   </Form>
+
                 )}
+
               </Formik>
+
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
+
+          </div>    
   );
-};
+}
 
 export default Register;
+
--- client/IMS-app/src/services/accountsService.js
+++ client/IMS-app/src/services/accountsService.js
@@ -10,13 +10,13 @@ export default class accountsService {
     return response.data;
   }
 
-  // async register(data) {
-  //   const response = await axios.post(
-  //     this.baseUrl + "/admin/user/create",
-  //     data
-  //   );
-  //   console.log("8888888888888888888888");
-  //   console.log(response.data);
-  //   return response.data;
-  // }
+  async register(data) {
+    const response = await axios.post(
+      this.baseUrl + "/admin/user/create",
+      data
+    );
+    console.log("8888888888888888888888");
+    console.log(response.data);
+    return response.data;
+  }
 }
